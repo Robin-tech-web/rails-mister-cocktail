4aae86d8aa26d8429c0c89622f2577d2f21e2744
Robin Weidlich <robinweidlich7@gmail.com>
2020-02-21 15:48:06 -0300

    refactor design

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,7 +5,7 @@ class DosesController < ApplicationController
   end
 
   def create
-  	@cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     @dose.save
@@ -13,6 +13,10 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
